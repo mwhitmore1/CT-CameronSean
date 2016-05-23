ce54350efc7c4c2a1624f4988ffe1fa25fe86054
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-05-23 09:31:40 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TheCountToOneHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Program.Counter < 100)
+            {
+                Program.Counter++;
+                if (Program.Counter % 3 == 0)
+                {
+                    if (Program.Counter % 5 == 0)
+                    {
+                        WriteLine("CameronSean");
+                    } else
+                    WriteLine("Cameron");
+                } else
+                if (Program.Counter % 5 == 0)
+                {
+                    WriteLine("Sean");
+                } else
+                {
+                    WriteLine(Program.Counter);
+                }
+                
+            }
+            ReadLine();
+        }
+        public static int Counter = 0;
+    }
+}
